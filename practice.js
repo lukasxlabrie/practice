b2b78e1e91e1985ad1d439e935eb223cea60d250
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-03 09:03:24 -0500

    task 2

--- practice.js
+++ practice.js
@@ -1,4 +1,3 @@
-let a = 1, b = 1;
+let a = 2;
 
-let c = ++a; // pre-increment, c = 1 increment by 1
-let d = b++; // post increment d is assigned value of b prior to incrment, so value is 1
+let x = 1 + (a *= 2);
