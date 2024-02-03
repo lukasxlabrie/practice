62accea6be8be01dd16e1adfe2245e4b49c844d4
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-03 09:07:27 -0500

    guesses part 1

--- practice.js
+++ practice.js
@@ -1,14 +1,14 @@
-"" + 1 + 0
-"" - 1 + 0
-true + false
+"" + 1 + 0 // "10"
+"" - 1 + 0 // "-10"
+true + false // comparing to obejects is always false
 6 / "3"
-"2" * "3"
-4 + 5 + "px"
-"$" + 4 + 5
-"4" - 2
-"4px" - 2
+"2" * "3" 
+4 + 5 + "px" // "45px"
+"$" + 4 + 5 // "$45"
+"4" - 2 
+"4px" - 2 //  "-24px"
 "  -9  " + 5
 "  -9  " - 5
-null + 1
-undefined + 1
-" \t \n" - 2
+null + 1 // NAN
+undefined + 1 // NAN 
+" \t \n" - 2 // "-2" (on a newc line tabbed)
