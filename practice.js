a68118a658fc7c1762daa0a35b8828dc172ecc38
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-03 08:59:45 -0500

    answers provided

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
 let a = 1, b = 1;
 
-let c = ++a; // ?
-let d = b++; // ?
+let c = ++a; // pre-increment, c = 1 increment by 1
+let d = b++; // post increment d is assigned value of b prior to incrment, so value is 1
