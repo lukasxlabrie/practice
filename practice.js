17581f15d6bfab9f9692f8920d56a0294b5e5c7b
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-02 19:46:32 -0500

    each line must be explained

--- practice.js
+++ practice.js
@@ -1,4 +1,14 @@
-let a = prompt("First number?", 1);
-let b = prompt("Second number?", 2);
-
-alert(a =+ b);
+"" + 1 + 0
+"" - 1 + 0
+true + false
+6 / "3"
+"2" * "3"
+4 + 5 + "px"
+"$" + 4 + 5
+"4" - 2
+"4px" - 2
+"  -9  " + 5
+"  -9  " - 5
+null + 1
+undefined + 1
+" \t \n" - 2
