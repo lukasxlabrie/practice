d30b34b8bb9c4b49815aa9e1a5cc741abd3c6a03
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-03 09:10:44 -0500

    gueeses take 2

--- practice.js
+++ practice.js
@@ -1,14 +1,14 @@
 "" + 1 + 0 // "10"
 "" - 1 + 0 // "-10"
 true + false // comparing to obejects is always false
-6 / "3"
-"2" * "3" 
+6 / "3"  // "2"
+"2" * "3" // "6"
 4 + 5 + "px" // "45px"
 "$" + 4 + 5 // "$45"
-"4" - 2 
+"4" - 2 // "4-2"
 "4px" - 2 //  "-24px"
-"  -9  " + 5
-"  -9  " - 5
+"  -9  " + 5 // " -9  + 5"
+"  -9  " - 5 // - - - + = so, 4
 null + 1 // NAN
 undefined + 1 // NAN 
 " \t \n" - 2 // "-2" (on a newc line tabbed)
