5e273c3d32284eadb28a38469c449c0fa65d346b
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-03 09:07:17 -0500

    tasks 3 round 2

--- practice.js
+++ practice.js
@@ -1,5 +1,14 @@
-let a = 2;
-
-let x = 1 + (a *= 2); // this is shorthand
-// Think of it like a (which = 2) tims 2, then we add 1
-//the answer is 5
+"" + 1 + 0
+"" - 1 + 0
+true + false
+6 / "3"
+"2" * "3"
+4 + 5 + "px"
+"$" + 4 + 5
+"4" - 2
+"4px" - 2
+"  -9  " + 5
+"  -9  " - 5
+null + 1
+undefined + 1
+" \t \n" - 2
