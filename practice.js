bdd5e47c292929b786542bad53a6535a4649ee56
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-02 19:54:10 -0500

    updated guesses

--- practice.js
+++ practice.js
@@ -1,14 +1,14 @@
 "" + 1 + 0 // 10, puts # in ""
-"" - 1 + 0 // -"10"
-true + false // invalid
-6 / "3" //2
+"" - 1 + 0 // -1, was correct the first time
+true + false // 1
+6 / "3" //2 
 "2" * "3" //6
 4 + 5 + "px"//9 px
-"$" + 4 + 5//$9
-"4" - 2 //2
+"$" + 4 + 5//$45, becomes string is string is first
+"4" - 2 //2 X NAN
 "4px" - 2 //2px
-"  -9  " + 5  //-4
-"  -9  " - 5 //-14
+"  -9  " + 5 //3
+"  -9  " - 5 // 4
 null + 1 //1
 undefined + 1 //1
 " \t \n" - 2 //2 on a new line and tabbed out
