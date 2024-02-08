0baab6399fb366cfde69ef036637c58f8127b911
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-08 18:31:00 -0500

    answers

--- practice.js
+++ practice.js
@@ -1,7 +1,7 @@
-5 > 4
-"apple" > "pineapple"
-"2" > "12"
-undefined == null
-undefined === null
-null == "\n0\n"
-null === +"\n0\n"
+5 > 4 // True *because 5 is greater than 4
+"apple" > "pineapple" // False *pineapple is the longer word
+"2" > "12" // False *12 is the longer string
+undefined == null // True *special case, since they are ntoh unique and eqaute to nothing
+undefined === null // False * these are different types and therfore differetn values
+null == "\n0\n" // False null is not converted here
+null === +"\n0\n" // True becuase null plus 0, will be equal to null plus 0
