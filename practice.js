9322041a7129abf79c2eead19e39ea22bc1a0544
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-09 18:14:37 -0500

    clean slate

--- practice.js
+++
@@ -1,7 +0,0 @@
-5 > 4 // True *because 5 is greater than 4
-"apple" > "pineapple" // False *pineapple is the longer word
-"2" > "12" // True, becuaew first char is basis of comparison
-undefined == null // True *special case, since they are ntoh unique and eqaute to nothing
-undefined === null // False * these are different types and therfore differetn values
-null == "\n0\n" // False null is not converted here
-null === +"\n0\n" // false different types can never be eqaul
