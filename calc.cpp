9ebb0b8acae03a62d3c08817568f06204fae902f
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-27 18:22:21 -0500

    corrected calc code and notes plus header

--- calc.cpp
+++ calc.cpp
@@ -1,35 +1,31 @@
-/*
- * Calculator.cpp
- *
- *  Date: 02/28/2024
- *  Author: Lukas Labrie
- */
+// Calculator.cpp
+// Date: 02/28/2024
+// Author: Lukas Labrie
 
 #include <iostream>
 
 using namespace std;
 
-void main()
+int main() // Changed void to int for main function
 {
-	char statement[100];
-	int op1, op2;
-	char operation;
-	char answer="Y"
-	while (answer=='y')
-	{
-		cout << "Enter expression" <<endl;
-		cin >> op2 >> operation >> op1;
-		if (operation == "+");
-			cout << op1 << " + " << op2 >> " = " << op1 + op2 << endl;
-		if (operation == '-');
-			cout >> op1 << " - " << op2 << " = " << op1 - op2 << endl;
-		if (operation == '*')
-			cout << op1 << " / " << op2 << " = " << op1 * op2 << endl
-		if (operation == '/')
-			cout << op1 << " * " << op2 << " = " << op1 / op2 << endl;
-
-		cout << "Do you wish to evaluate another expression? " << endl;
-		cin >> answer;
-	}
+    char statement[100];
+    int op1, op2;
+    char operation;
+    char answer = 'Y'; // Initialized answer to 'Y' and changed double quotes to single quotes
+    while (answer == 'Y' || answer == 'y') // Added curly braces and fixed loop condition
+    {
+        cout << "Enter expression" << endl;
+        cin >> op2 >> operation >> op1;
+        if (operation == '+') // Removed semicolon after if statement
+            cout << op1 << " + " << op2 << " = " << op1 + op2 << endl; // Fixed order of operands
+        if (operation == '-') // Removed semicolon after if statement
+            cout << op1 << " - " << op2 << " = " << op1 - op2 << endl; // Fixed order of operands
+        if (operation == '*') // Added missing semicolon at the end of the line
+            cout << op1 << " * " << op2 << " = " << op1 * op2 << endl; // Fixed order of operands and changed '/' to '*'
+        if (operation == '/') // Removed semicolon after if statement and fixed '/' to '*'
+            cout << op1 << " / " << op2 << " = " << op1 / op2 << endl; // Fixed order of operands and changed '*' to '/'
+        cout << "Do you wish to evaluate another expression? " << endl;
+        cin >> answer;
+    }
+    return 0; // Added return statement
 }
-
