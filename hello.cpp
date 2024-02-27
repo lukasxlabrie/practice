fe4b1116cfb48320c670199afd06bfc036850af3
misosucky <126988962+Misosucky@users.noreply.github.com>
2024-02-27 18:05:25 -0500

    gello world in cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// Lukas Labrie
+// 02/27/2024
+// print "Hello, World!" in C++
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Output "Hello, World!"
+    return 0;
+}
